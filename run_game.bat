ef5651e5b1f95b7e8061feccc3e2d107a8170d88
gtmp <g.t.m.p@yandex.ru>
2018-12-12 23:39:28 +0300

    forgot to add this tools

---
+++ run_game.bat
@@ -0,0 +1,1 @@
+halite.exe --replay-directory replays/ -vvv --width 32 --height 32 "python gelberB.py" "python gelberB.py"
---
+++ run_game.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./halite --replay-directory replays/ -vvv --width 32 --height 32 "python3 gelberB.py" "python3 gelberB.py"
