b05dc34b37076bb4ad0305747e6386089491fd1d
Anton O. Burygin <a.burygin@norsi-trans.ru>
2018-12-13 18:40:22 +0300

    пуш после работы

--- gelberB.py
+++ gelberB.py
@@ -1,64 +1,53 @@
 #!/usr/bin/env python3
-# Python 3.6
-
-# Import the Halite SDK, which will let you interact with the game.
 import hlt
-
-# This library contains constant values.
 from hlt import constants
-
-# This library contains direction metadata to better interface with the game.
 from hlt.positionals import Direction
-
-# This library allows you to generate random numbers.
 import random
-
-# Logging allows you to save messages for yourself. This is required because the regular STDOUT
-#   (print statements) are reserved for the engine-bot communication.
 import logging
 
 """ <<<Game Begin>>> """
-
-# This game object contains the initial game state.
 game = hlt.Game()
-# At this point "game" variable is populated with initial map data.
-# This is a good place to do computationally expensive start-up pre-processing.
-# As soon as you call "ready" function below, the 2 second per turn timer will start.
-game.ready("MyPythonBot")
-
-# Now that your bot is initialized, save a message to yourself in the log file with some important information.
-#   Here, you log here your id, which you can always fetch from the game object by using my_id.
+all_cells_collection = [hlt.Position(i, y) for i in range(game.game_map.width) for y in range(game.game_map.height)]
+game.ready("simple_rad_v1")
 logging.info("Successfully created bot! My Player ID is {}.".format(game.my_id))
-
 """ <<<Game Loop>>> """
 
 while True:
-    # This loop handles each turn of the game. The game object changes every turn, and you refresh that state by
-    #   running update_frame().
     game.update_frame()
-    # You extract player metadata and the updated map metadata here for convenience.
     me = game.me
     game_map = game.game_map
+    halite_map = {cell: game_map[cell].halite_amount for cell in all_cells_collection}
+    # logging.info("{}.".format(all_cells_collection))
+    # logging.info("{}.".format(halite_map))
 
-    # A command queue holds all the commands you will run this turn. You build this list up and submit it at the
-    #   end of the turn.
     command_queue = []
-
+    direction_order = [Direction.North, Direction.South, Direction.East, Direction.West, Direction.Still]
     for ship in me.get_ships():
-        # For each of your ships, move randomly if the ship is on a low halite location or the ship is full.
-        #   Else, collect halite.
+
+        position_options = ship.position.get_surrounding_cardinals() + [ship.position]
+
+        position_dict = {}
+        halite_dict = {}
+
+        for n, direction in enumerate(direction_order):
+            position_dict[direction] = position_options[n]
+
+        for direction in position_dict:
+            position = position_dict[direction]
+            halite_dict[position] = game_map[position].halite_amount
+
+
+        logging.info("{}.".format(position_options))
+        logging.info("{}.".format(halite_dict))
+
         if game_map[ship.position].halite_amount < constants.MAX_HALITE / 10 or ship.is_full:
             command_queue.append(
                 ship.move(
-                    random.choice([ Direction.North, Direction.South, Direction.East, Direction.West ])))
+                    random.choice([Direction.North, Direction.South, Direction.East, Direction.West])))
         else:
             command_queue.append(ship.stay_still())
 
-    # If the game is in the first 200 turns and you have enough halite, spawn a ship.
-    # Don't spawn a ship if you currently have a ship at port, though - the ships will collide.
     if game.turn_number <= 200 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
         command_queue.append(me.shipyard.spawn())
 
-    # Send your moves back to the game environment, ending this turn.
     game.end_turn(command_queue)
-
---
+++ tools/utils.py
@@ -0,0 +1,4 @@
+import hlt
+
+def find_max_halite(ship_point, halite_map, road_map):
+    pass
